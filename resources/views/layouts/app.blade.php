cbac1b8f0a45d5ea75112220a721f4faff990e5f
Juan Dausa <juandausa@gmail.com>
2017-08-30 22:39:48 -0300

    Se agrega el abm de tareas

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!-- // resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">        
+        <!-- CSS And JavaScript -->
+    </head>
+
+    <body>
+        <br />
+        <div class="container-fluid">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
